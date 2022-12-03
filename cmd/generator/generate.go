b7b3f76aea881118c519c4ca6475f819aa04a0f6
Tyson Gern <tyson@initialcapacity.io>
2022-12-02 19:17:10 -0700

    Extract generator

--- cmd/generator/generate.go
+++ cmd/generator/generate.go
@@ -1,76 +1,13 @@
 package main
 
 import (
-	"bytes"
-	"io"
-	"log"
-	"os"
-	"path"
-	"path/filepath"
-	"strings"
+	"github.com/tygern/generator/internal/generator"
 )
 
-type SourceFile struct {
-	Path     string
-	FileInfo os.FileInfo
-}
-
 func main() {
-	root := "."
+	g := generator.New(".")
 
-	buildDir := path.Join(root, "build")
-	err := os.RemoveAll(buildDir)
-	if err != nil {
-		log.Fatalf("Unable to delete build directory")
-	}
-
-	templatePath := path.Join(root, "template.html")
-
-	sourceFiles := make([]SourceFile, 0)
-
-	_ = filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
-		if path == templatePath {
-			return nil
-		}
-
-		sourceFiles = append(sourceFiles, SourceFile{Path: path, FileInfo: info})
-		return nil
-	})
-
-	templateBytes, _ := os.ReadFile(templatePath)
-
-	for _, f := range sourceFiles {
-		destination := path.Join(buildDir, f.Path)
-
-		if f.FileInfo.IsDir() {
-			_ = os.MkdirAll(destination, 0700)
-			continue
-		}
-
-		if strings.HasSuffix(f.Path, ".html") {
-			input, _ := os.ReadFile(f.Path)
-			input = bytes.ReplaceAll(templateBytes, []byte("<#content/>"), input)
-			err := os.WriteFile(destination, input, 0700)
-			if err != nil {
-				log.Fatalf("Unable to copy %s: %s", f.Path, err)
-			}
-		} else {
-			from, err := os.Open(f.Path)
-			if err != nil {
-				log.Fatal(err)
-			}
-			defer from.Close()
-
-			to, err := os.OpenFile(destination, os.O_RDWR|os.O_CREATE, 0700)
-			if err != nil {
-				log.Fatal(err)
-			}
-			defer to.Close()
-
-			_, err = io.Copy(to, from)
-			if err != nil {
-				log.Fatal(err)
-			}
-		}
-	}
+	g.ClearBuildDirectory()
+	sourceFiles := g.FindSourceFiles()
+	g.Build(sourceFiles)
 }
---
+++ internal/generator/generator.go
@@ -0,0 +1,122 @@
+package generator
+
+import (
+	"bytes"
+	"io"
+	"log"
+	"os"
+	"path"
+	"path/filepath"
+	"strings"
+)
+
+type Generator struct {
+	rootPath     string
+	templatePath string
+	buildPath    string
+}
+
+type SourceFile struct {
+	Path     string
+	FileInfo os.FileInfo
+}
+
+func New(rootPath string) Generator {
+	return Generator{
+		rootPath:     rootPath,
+		templatePath: path.Join(rootPath, "template.html"),
+		buildPath:    path.Join(rootPath, "build"),
+	}
+}
+
+func (g Generator) ClearBuildDirectory() {
+	err := os.RemoveAll(g.buildPath)
+	if err != nil {
+		log.Fatalf("Unable to delete build directory: %s", err)
+	}
+}
+
+func (g Generator) FindSourceFiles() []SourceFile {
+	sourceFiles := make([]SourceFile, 0)
+
+	err := filepath.Walk(g.rootPath, func(path string, info os.FileInfo, err error) error {
+		if path == g.templatePath {
+			return nil
+		}
+
+		sourceFiles = append(sourceFiles, SourceFile{Path: path, FileInfo: info})
+		return nil
+	})
+	if err != nil {
+		log.Fatalf("Unable find source files: %s", err)
+	}
+
+	return sourceFiles
+}
+
+func (g Generator) Build(sourceFiles []SourceFile) {
+	templateBytes := g.readTemplate()
+
+	for _, sourceFile := range sourceFiles {
+		destination := path.Join(g.buildPath, sourceFile.Path)
+
+		if sourceFile.FileInfo.IsDir() {
+			makeDirectory(destination)
+			continue
+		}
+
+		if strings.HasSuffix(sourceFile.Path, ".html") {
+			applyTemplate(sourceFile, templateBytes, destination)
+		} else {
+			copyFile(sourceFile, destination)
+		}
+	}
+
+}
+
+func (g Generator) readTemplate() []byte {
+	templateBytes, err := os.ReadFile(g.templatePath)
+	if err != nil {
+		log.Fatalf("Unable to read template %s: %s", g.templatePath, err)
+	}
+	return templateBytes
+}
+
+func makeDirectory(destination string) {
+	err := os.MkdirAll(destination, 0700)
+	if err != nil {
+		log.Fatalf("Unable to create directory %s: %s", destination, err)
+	}
+}
+
+func applyTemplate(sourceFile SourceFile, templateBytes []byte, destination string) {
+	input, _ := os.ReadFile(sourceFile.Path)
+	input = bytes.ReplaceAll(templateBytes, []byte("<#content/>"), input)
+	err := os.WriteFile(destination, input, 0700)
+	if err != nil {
+		log.Fatalf("Unable to copy %s: %s", sourceFile.Path, err)
+	}
+}
+
+func copyFile(sourceFile SourceFile, destination string) {
+	from, err := os.Open(sourceFile.Path)
+	if err != nil {
+		log.Fatalf("Unable to open %s: %s", sourceFile.Path, err)
+	}
+	defer func(from *os.File) {
+		_ = from.Close()
+	}(from)
+
+	to, err := os.OpenFile(destination, os.O_RDWR|os.O_CREATE, 0700)
+	if err != nil {
+		log.Fatalf("Unable to create %s: %s", destination, err)
+	}
+	defer func(to *os.File) {
+		_ = to.Close()
+	}(to)
+
+	_, err = io.Copy(to, from)
+	if err != nil {
+		log.Fatalf("Unable to copy %s: %s", sourceFile.Path, err)
+	}
+}
